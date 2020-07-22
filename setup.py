2d7a3e6e6fdf83d52410ad2aee2535ffb4ef0fc9
roger <roger.herikstad@gmail.com>
2020-07-22 10:32:45 +0800

    Adds setuppy

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(name="MatplotlibHelpers",
+      version="0.0.1",
+      description="Various helpers for Matplotlib",
+      url="https://github.com/grero/MatplotlibHelpers.git",
+      author="Roger Herikstad",
+      author_email="roger.herikstad@gmail.com",
+      license="MIT",
+      packages=["MatplotlibHelpers"],
+      include_package_data=True
+    )
